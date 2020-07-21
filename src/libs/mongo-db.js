0029d2bfa2e657d02f973f30495e8701788c33c7
villa999 <43264383+villa999@users.noreply.github.com>
2020-07-21 20:13:36 +0700

    Update mongo-db.js

--- src/libs/mongo-db.js
+++ src/libs/mongo-db.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose")
+const config = require("config")
+
 require("../apps/models/category")
 require("../apps/models/product")
 require("../apps/models/user")
 require("../apps/models/comment")
 //const uris = "mongodb://127.0.0.1:27017/Vietpro_mongodb"
-const uris = process.env.MONGODB_URI
-//const uris = "mongodb://mongodb/Vietpro_mongodb"
+const uris = config.get("mongo.uri");
 
-//uris: link connect toi mongodb
-mongoose.connect(uris)
+mongoose.connect(uris);
